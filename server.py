f600ad76705075b37ad2e0d86d6a17eaa377ab9c
cnmurakami <caionmurakami@gmail.com>
2024-03-19 23:34:52 -0300

    Verificação de dados e status code

--- server.py
+++ server.py
@@ -15,29 +15,47 @@ def home():
 
 @app.route('/register', methods=['GET','POST'])
 def register():
+    status_code=599
     try:
-        clientName = request.form['clientName']
-        CPF = request.form['CPF']
-        RazaoSocial = request.form['RazaoSocial']
-        CNPJ = request.form['CNPJ']
-        Telephone = request.form['Telephone']
-        Cellphone = request.form['Cellphone']
-        Email1 = request.form['Email1']
-        Email2 = request.form['Email2']
-        CEP = request.form['CEP']
-        Logradouro = request.form['Logradouro']
-        Numero = request.form['Numero']
-        Complemento = request.form['Complemento']
-        brazilianStates = request.form['brazilianStates']
-        city = request.form['city']
-        if ((clientName and CPF) or (RazaoSocial and CNPJ)) and (Telephone or Cellphone) and (Email1 or Email2) and CEP:
+        client_name = request.form['clientName']
+        cpf = request.form['CPF']
+        razao_social = request.form['RazaoSocial']
+        cnpj = request.form['CNPJ']
+        telefone = request.form['Telephone']
+        celular = request.form['Cellphone']
+        email1 = request.form['Email1']
+        email2 = request.form['Email2']
+        cep = request.form['CEP']
+        logradouro = request.form['Logradouro']
+        numero = request.form['Numero']
+        complemento = request.form['Complemento']
+        estado = request.form['brazilianStates']
+        cidade = request.form['city']
+        endereco = f'{logradouro}, {numero}, {complemento}, {cidade} - {estado}, CEP {cep}'
+        if ((client_name and cpf) or (razao_social and cnpj)) and (telefone or celular) and (email1 or email2) and cep:
+            status_code=550
             conn = mysql.connection
             cursor = conn.cursor()
-            cursor.execute('insert into cliente (cpf, cnpj, nome, razao_social, endereco, telefone, email) VALUES (%s, %s, %s, %s, %s, %s, %s)', (CPF, CNPJ, clientName, RazaoSocial, (f'{Logradouro}, {Numero}, {Complemento}, {city} - {brazilianStates}, CEP {CEP}'), (f'{Telephone}/{Cellphone}'), Email1))
-            conn.commit()
-            return render_template('vehicle_registration.html')
+            cursor.execute('select * from cliente where cpf = %s or cnpj = %s', (cpf, cnpj))
+            result = cursor.fetchall()
+            cursor.close()
+            if (len(result)>0):
+                status_code=450
+                raise
+            else:
+                status_code=551
+                cursor = conn.cursor()
+                arg=(cpf, cnpj, client_name, razao_social, endereco, telefone, email1)
+                cursor.callproc('inserir_cliente',arg)
+                #cursor.execute('insert into cliente (cpf, cnpj, nome, razao_social, endereco, telefone, email) VALUES (%s, %s, %s, %s, %s, %s, %s)', (CPF, CNPJ, clientName, RazaoSocial, (f'{Logradouro}, {Numero}, {Complemento}, {city} - {brazilianStates}, CEP {CEP}'), (f'{Telephone}/{Cellphone}'), Email1))
+                cursor.close()
+                conn.commit()
+            return render_template('vehicle_registration.html'), 200
+        else:
+            status_code=460
+            raise
     except:
-        return render_template('register.html'),500
+        return render_template('register.html'), status_code
 
 @app.route('/vehicleregistration', methods=['GET','POST'])
 def vehicleregistration():
